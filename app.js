8b2e5cdc3e7165182888036997c2c4df30021501
comrade-sergei <sergeiskoropadski@gmail.com>
2022-04-08 18:18:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const prefix = '!';
 client.on("ready", function(){
   console.log(client.user.username);
   client.user.setPresence({
-    status: 'idle',
+    status: 'online',
     activity: {
-      name: "with YRDSB",
+      name: "with swagger.json",
       type: 'PLAYING'
     }
   })
@@ -26,18 +26,16 @@ client.on("message", (message) =>{
 
   if (command == "help") {
     const helpEmbed = new Discord.MessageEmbed()
-    .setColor('#FF0000')
-    .setTitle('**Nachricht von der Gestapo**')
-    .setAuthor('Großdeutsches Reich', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Reichsadler_Deutsches_Reich_%281935%E2%80%931945%29.svg/220px-Reichsadler_Deutsches_Reich_%281935%E2%80%931945%29.svg.png', 'https://en.wikipedia.org/wiki/Nazi_Germany')
-    .setDescription('Am 30. Januar 1933 zog ich in die Wilhelmstraße ein, erfüllt von tiefster Sorge für die Zukunft meines Volkes. Heute — sechs Jahre später — kann ich zu dem ersten Reichstag Großdeutschlands sprechen! Wahrlich, wir vermögen vielleicht mehr als eine andere Generation den frommen Sinn des Ausspruches zu ermessen: Welch eine Wendung durch Gottes Fügung!')
-    .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/National_Socialist_swastika.svg/1200px-National_Socialist_swastika.svg.png')
-    .setImage('https://cdn.britannica.com/58/129958-050-C3FE2DD2/Adolf-Hitler-1933.jpg')
+    .setColor('#154fad')
+    .setTitle('**How to use the TeachAssist Bot**')
+    .setDescription(`
+      Sussus
+    `)
     .setTimestamp()
-    .setFooter('Großdeutsches Reich', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Reichsadler_Deutsches_Reich_%281935%E2%80%931945%29.svg/220px-Reichsadler_Deutsches_Reich_%281935%E2%80%931945%29.svg.png', 'https://en.wikipedia.org/wiki/Nazi_Germany')
+    .setFooter('TeachAssist Bot', 'https://cdn.discordapp.com/attachments/962109342407082067/962114089058992209/unnamed.jpg')
 
     message.channel.send(helpEmbed);
-    message.delete();
   }
-}); 
+});
 
 client.login(process.env.token);
