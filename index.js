41a0b5f0638bf4c64d11de8d2cc0249d81017120
comrade-sergei <sergeiskoropadski@gmail.com>
2022-04-09 14:43:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,9 @@
 Omid Yaqub and Dylan Burke
 04/08/2022
 
-Last Updated: 04/09/2022 @ 1:01 AM EST
+Last Updated: 04/09/2022 @ 2:07 AM EST
 
 https://github.com/comrade-sergei/weather-bot
-*/
-
-
-/* READ AND THEN DELETE....
-  USE GOOGLE TRANSLATE API TO MAKE IT WORK IN ANY LANGUAGE ((USE ARGS FOR IDENTIFICATION)).
 */
 
 // Modular Variables
@@ -22,7 +17,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 // Variables
 const prefix = '!';
 const token = process.env.token;
-var language = 'de';
+var language = 'en';
 
 // Intialization
 const client = new Client({ 
@@ -34,8 +29,15 @@ const client = new Client({
 
 client.on("ready", function(){
   console.log(client.user.tag);
+
+  client.user.setPresence({
+    status: "online"
+  });
+
+  client.user.setActivity('Amongus Rap', { type: 'LISTENING' });
 });
 
+// Function
 client.on("messageCreate", (message) => {
 
   if (!message.content.startsWith(prefix) || message.author.bot) return; // put this into use.
@@ -43,12 +45,16 @@ client.on("messageCreate", (message) => {
   const args = message.content.slice(1).trim().split(' ');
   const command = args.shift().toLowerCase();
 
-  if(command == "ping") {
-    message.reply(`${Date.now() - message.createdTimestamp}ms.`); // fix times. currently == - numbers.
+  if(command == "help") {
+    message.reply('```!help, !temp, !dc, !translate, !set, !observationtime, !windspeed, !skytext, !date, !humidity, !amongus, !rap```') // make it look better, use embeds.
   }
 
-  if(command == "help") {
-    message.reply('```!ping, !help, !temp, !dc, !translate, !set, !observationtime, !windspeed, !skytext, !date, !humidity```') // make it look better, use embeds.
+  if(command == "amongus") {
+    message.reply('https://www.youtube.com/watch?v=2AeEd195SG8');
+  }
+
+  if(command == "rap") {
+    message.reply('https://www.youtube.com/watch?v=T59N3DPrvac');
   }
 
   if(command == 'temp' || command == 'temperature') {
@@ -75,76 +81,76 @@ client.on("messageCreate", (message) => {
 
 if(command == 'windspeed') {
   if (args.at(0) != null) {
-  weather.find({search: args.at(0), degreeType: 'C'}, function(error, result) { // add args support (for multiple places).
-   
-    if(error) console.log(error);
-   
-    var stringified = JSON.stringify(result, null, 2);
-    var parsed = JSON.parse(stringified);
-    if (parsed[0] != undefined){
-      translate("The windspeed in " + args.at(0) + " is " + parsed[0].current.windspeed , {to: language}).then(response => {
-          message.reply(response.text);
-      }); 
+    weather.find({search: args.at(0), degreeType: 'C'}, function(error, result) { // add args support (for multiple places).
+    
+      if(error) console.log(error);
+    
+      var stringified = JSON.stringify(result, null, 2);
+      var parsed = JSON.parse(stringified);
+      if (parsed[0] != undefined){
+        translate("The windspeed in " + args.at(0) + " is " + parsed[0].current.windspeed , {to: language}).then(response => {
+            message.reply(response.text);
+        }); 
+    } else {
+      message.reply("Enter a valid city with no spaces.");
+    }
+    });
   } else {
-    message.reply("Enter a valid city with no spaces.");
+    message.reply("Enter a city.");
   }
-  });
-} else {
-  message.reply("Enter a city.");
-}
 }
 
 if(command == 'observationtime') {
   if (args.at(0) != null) {
-  weather.find({search: args.at(0), degreeType: 'C'}, function(error, result) { // add args support (for multiple places).
-   
-    if(error) console.log(error);
-   
-    var stringified = JSON.stringify(result, null, 2);
-    var parsed = JSON.parse(stringified);
-    if (parsed[0] != undefined){
-      translate("The time in " + args.at(0) + " is " + parsed[0].current.observationtime, {to: language}).then(response => {
-          message.reply(response.text);
-      }); 
+    weather.find({search: args.at(0), degreeType: 'C'}, function(error, result) { // add args support (for multiple places).
+    
+      if(error) console.log(error);
+    
+      var stringified = JSON.stringify(result, null, 2);
+      var parsed = JSON.parse(stringified);
+      if (parsed[0] != undefined){
+        translate("The observation time in " + args.at(0) + " is " + parsed[0].current.observationtime, {to: language}).then(response => {
+            message.reply(response.text);
+        }); 
+    } else {
+      message.reply("Enter a valid city with no spaces.");
+    }
+    });
   } else {
-    message.reply("Enter a valid city with no spaces.");
+    message.reply("Enter a city.");
   }
-  });
-} else {
-  message.reply("Enter a city.");
-}
 }
 
 if(command == 'skytext') {
   if (args.at(0) != null) {
-  weather.find({search: args.at(0), degreeType: 'C'}, function(error, result) { // add args support (for multiple places).
-   
-    if(error) console.log(error);
-   
-    var stringified = JSON.stringify(result, null, 2);
-    var parsed = JSON.parse(stringified);
-    if (parsed[0] != undefined){
-      translate("it is " + parsed[0].current.skytext + " " +"in "  + args.at(0), {to: language}).then(response => {
-          message.reply(response.text);
-      }); 
+    weather.find({search: args.at(0), degreeType: 'C'}, function(error, result) { // add args support (for multiple places).
+    
+      if(error) console.log(error);
+    
+      var stringified = JSON.stringify(result, null, 2);
+      var parsed = JSON.parse(stringified);
+      if (parsed[0] != undefined){
+        translate("It's " + parsed[0].current.skytext + " in "  + args.at(0), {to: language}).then(response => {
+            message.reply(response.text);
+        }); 
+    } else {
+      message.reply("Enter a valid city with no spaces.");
+    }
+    });
   } else {
-    message.reply("Enter a valid city with no spaces.");
+    message.reply("Enter a city.");
   }
-  });
-} else {
-  message.reply("Enter a city.");
-}
 }
 
 if(command == 'date') {
-  weather.find({search: 'Toronto, CA', degreeType: 'C'}, function(error, result) { // add args support (for multiple places).
+  weather.find({search: 'Toronto, CA', degreeType: 'C'}, function(error, result) {
    
     if(error) console.log(error);
    
     var stringified = JSON.stringify(result, null, 2);
     var parsed = JSON.parse(stringified);
     if (parsed[0] != undefined){
-      translate("the date is " + parsed[0].current.date, {to: language}).then(response => {
+      translate("The date is " + parsed[0].current.date + " in Toronto", {to: language}).then(response => {
           message.reply(response.text);
       }); 
   } else {
@@ -153,36 +159,39 @@ if(command == 'date') {
   });
 }
 
-if(command == 'humdity') {
+if(command == 'humidity') {
   if (args.at(0) != null) {
-  weather.find({search: args.at(0), degreeType: 'C'}, function(error, result) { // add args support (for multiple places).
-   
-    if(error) console.log(error);
-   
-    var stringified = JSON.stringify(result, null, 2);
-    var parsed = JSON.parse(stringified);
-    if (parsed[0] != undefined){
-      translate("the humidity is" + parsed[0].current.humidity + "in " + args.at(0), {to: language}).then(response => {
-          message.reply(response.text);
-      }); 
+    weather.find({search: args.at(0), degreeType: 'C'}, function(error, result) {
+    
+      if(error) console.log(error);
+    
+      var stringified = JSON.stringify(result, null, 2);
+      var parsed = JSON.parse(stringified);
+      if (parsed[0] != undefined){
+        translate("The humidity in " + args.at(0) + " is " + parsed[0].current.humidity, {to: language}).then(response => {
+            message.reply(response.text);
+        }); 
+    } else {
+      message.reply("Enter a valid city with no spaces.");
+    }
+    });
   } else {
-    message.reply("Enter a valid city with no spaces.");
+    message.reply("Enter a city.");
   }
-  });
-} else {
-  message.reply("Enter a city.");
-}
 }
 
   if(command == "set") {
-    console.log(language);
-    message.reply('Language set to ' + args.at(0));
-    language = args.at(0);
-    console.log(language);
+    if(args.at(0) != null) {
+      message.reply('Language set to ' + args.at(0));
+      language = args.at(0);
+    } else {
+      message.reply('Enter a valid language code.');
+      message.channel.send("For reference: https://cloud.google.com/translate/docs/languages");
+    }
   }
 
   if(command == "dc") {
-    message.reply("Disconnected.");
+    message.channel.send("Disconnected.");
     client.destroy();
     process.exit(0);
   }
