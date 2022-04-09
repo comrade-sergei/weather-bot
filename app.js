acc636fcf81250ee281a88dd9bbf9d5062902c9d
comrade-sergei <sergeiskoropadski@gmail.com>
2022-04-08 20:29:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Starter
 const Discord = require('discord.js');
-const axios = require('axios');
+const weather = require('weather-js');
 const client = new Discord.Client();
 const prefix = '!';
 
@@ -52,12 +52,27 @@ client.on("message", (message) =>{
   }
 
   if(command == "getForecast") {
-      message.channel.send("City entered");
+      weather.find({search: args.join(" "), degreeType: `C`}, function (error, result) {
+        if(error) return message.channel.send(error);
+        if(!args[0]) return message.channel.send('Please specify a location!')
 
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=${process.env.weathertoken}`;
-      const response = await axios.get(url);
-      const city = response.data;
-      message.channel.send(city);
+        var current = result[0].current;
+        var location = result[0].location;
+
+        const embed = new Discord.MessageEmbed()
+        .setColor(0x111111)
+        .setAuthor(`Weather forecast for ${current.observationpoint}`)
+        .setThumbnail(current.imageUrl)
+        .setDescription(`**${current.skytext}**`)
+        .addField('TimeZone', `UTC ${location.timezone}`, true)
+        .addField('Degree Type', 'Celcius', true)
+        .addField('Temperature', `${current.temperature}°`, true) 
+        .addField('Wind', `${current.winddisplay}`, true)
+        .addField('Feels Like', `${current.feelslike}°`, true)
+        .addField('Humidity', `${current.humidity}%`, true)
+
+        message.channel.send(embed)
+      })
     }
 });
 
